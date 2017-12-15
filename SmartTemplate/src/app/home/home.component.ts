21ad80b79d8bed10729c04946a91faece193bf42
Luong Nguyen Dinh <luongnd169@gmail.com>
2017-12-15 22:49:44 +0700

    done scroll spy

--- SmartTemplate/src/app/home/home.component.ts
+++ SmartTemplate/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Renderer } from '@angular/core';
+import { Component, OnInit, Renderer, HostListener, ElementRef } from '@angular/core';
 import { HomeService } from './home.service';
 
 @Component({
@@ -36,11 +36,29 @@ export class HomeComponent implements OnInit {
 
   goToSection(section: string, event: any) {
     document.querySelector('#' + section).scrollIntoView();
+    this.activeElement(event.target.parentElement);
+  }
+
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    const menuItems = document.getElementById('nav-list').children;
+    const infoItems = ['section1', 'section2', 'section3', 'section4', 'section5', 'section6'];
+    for (let i = 0; i < infoItems.length; i++) {
+      if (document.querySelector('#' + infoItems[i]).getBoundingClientRect().top <= 0) {
+        this.activeElement(menuItems[i]);
+      }
+    }
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+      this.activeElement(menuItems[menuItems.length - 2]);
+    }
+  }
+
+  activeElement(element) {
     const menuItems = document.getElementById('nav-list').children;
     for (let i = 0; i < menuItems.length; i++) {
       menuItems.item(i).classList.remove('active-menu');
     }
-    this.renderer.setElementClass(event.target.parentElement, 'active-menu', true);
+    this.renderer.setElementClass(element, 'active-menu', true);
   }
 
 }
