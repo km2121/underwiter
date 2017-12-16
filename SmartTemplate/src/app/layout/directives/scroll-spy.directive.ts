26113573c4f31a5781af7873d9c7fc9dd70c7dad
Luong Nguyen <luongnd169@gmail.com>
2017-12-17 00:04:30 +0700

    complete scroll spy

--- SmartTemplate/src/app/layout/directives/scroll-spy.directive.ts
+++ SmartTemplate/src/app/layout/directives/scroll-spy.directive.ts
@@ -8,19 +8,39 @@ import { DOCUMENT } from '@angular/platform-browser';
 export class ScrollSpyDirective {
     @Input() appScrollSpy: number;
     @Input('className') className: string;
+    lastScrollTop: number;
+    scrollUp: boolean;
     constructor(
         private el: ElementRef,
         @Inject(DOCUMENT) private document: Document,
         private renderer: Renderer
     ) {
+        this.lastScrollTop = 0;
     }
 
     @HostListener('window:scroll')
     onWindowScroll() {
-        if (this.document.documentElement.scrollTop > this.appScrollSpy) {
-            this.renderer.setElementClass(this.el.nativeElement, this.className, true);
+        if (window.scrollY > this.lastScrollTop) {
+            this.scrollUp = false;
         } else {
-            this.renderer.setElementClass(this.el.nativeElement, this.className, false);
+            this.scrollUp = true;
+        }
+        this.lastScrollTop = window.scrollY;
+        const childEle = this.el.nativeElement.children[0];
+        if (childEle.offsetHeight < window.innerHeight) {
+            if (this.document.documentElement.scrollTop > this.appScrollSpy) {
+                this.renderer.setElementClass(this.el.nativeElement, this.className, true);
+            } else {
+                this.renderer.setElementClass(this.el.nativeElement, this.className, false);
+            }
+        } else {
+            const revertClass = this.className + '-revert';
+            if (window.innerHeight - (childEle.offsetHeight + childEle.getBoundingClientRect().top) > 20 && this.scrollUp === false) {
+                this.renderer.setElementClass(this.el.nativeElement, revertClass, true);
+            } else if (window.scrollY < window.innerHeight - (childEle.offsetHeight + childEle.getBoundingClientRect().top - 100)
+                        && this.scrollUp === true) {
+                this.renderer.setElementClass(this.el.nativeElement, revertClass, false);
+            }
         }
     }
 }
