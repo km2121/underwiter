310d5f632bf94a9782617c8445fd50df1fec17d4
Luong Nguyen <luongnd169@gmail.com>
2017-12-28 21:42:54 +0700

    update json

---
+++ SmartTemplate/src/app/shared/constant/common.constant.ts
@@ -0,0 +1,9 @@
+export class ComponentConstant {
+    public static INPUT = 'textBox';
+    public static DATEPICKER = 'datepicker';
+    public static DROPDOWN = 'dropdownBox';
+    public static MULTI_SELECT_DROPDOWN = 'multiSelectBox';
+    public static RADIO_GROUP = 'radioBox';
+    public static CHECK_BOX = 'checkBox';
+    public static TEXT_AREA = 'textarea';
+}
--- SmartTemplate/src/app/shared/index.ts
+++ SmartTemplate/src/app/shared/index.ts
@@ -1,2 +1,3 @@
 export * from './shared-libs.module';
 export * from './model/data-interface';
+export * from './constant/common.constant';
